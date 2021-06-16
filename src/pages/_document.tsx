c3921d0496eee6e7c98d6ca9330026500f97621a
Julia Ramos <juliaramosguedes@gmail.com>
2021-06-16 12:32:29 -0300

    add pixel facebook

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Document, {DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import React from "react";
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
                         sheet.collectStyles(<App {...props} />),
-                })
+                });
 
-            const initialProps = await Document.getInitialProps(ctx)
+            const initialProps = await Document.getInitialProps(ctx);
             return {
                 ...initialProps,
                 styles: (
@@ -23,18 +23,38 @@ export default class MyDocument extends Document {
                         {sheet.getStyleElement()}
                     </>
                 ),
-            }
+            };
         } finally {
-            sheet.seal()
+            sheet.seal();
         }
     }
 
     render(): JSX.Element {
-        return(
+        return (
             <Html lang="pt-BR">
                 <Head>
-                    {/* Início - Global site tag (gtag.js) - Google Analytics */}
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-63SPKN7LVL" />
+                    {/* Facebook Pixel Code */}
+                    <script dangerouslySetInnerHTML={{
+                        __html: `!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '263906128858074');
+      fbq('track', 'PageView');`,
+                    }}
+                    />
+                    <noscript dangerouslySetInnerHTML={{
+                        __html: `<img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=263906128858074&ev=PageView&noscript=1" />`,
+                    }}
+                    />
+                    {/* End - Facebook Pixel Code */}
+                    {/* Global site tag (gtag.js) - Google Analytics */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-63SPKN7LVL"/>
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
@@ -45,24 +65,24 @@ export default class MyDocument extends Document {
                             `,
                         }}
                     />
-                    {/* Fim - Global site tag (gtag.js) - Google Analytics */}
-                    <meta charSet="utf-8" />
+                    {/* End - Global site tag (gtag.js) - Google Analytics */}
+                    <meta charSet="utf-8"/>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;1,300&display=swap"
                         rel="stylesheet"
                     />
-                    <link rel="canonical" href="https://www.maheoi.com" />
-                    <link rel="icon" href="favicon.ico" />
-                    <meta name="robots" content="index, follow" />
+                    <link rel="canonical" href="https://www.maheoi.com"/>
+                    <link rel="icon" href="favicon.ico"/>
+                    <meta name="robots" content="index, follow"/>
                     <meta
                         name="description"
                         content="Relaxe profundamente e entre em equilíbrio com o reiki xamânico ou adquira mais
                         consciência sobre o seu universo com o tarot. Agende sua consulta online!"
                     />
-                    <meta name="author" content="Julia Ramos" />
+                    <meta name="author" content="Julia Ramos"/>
                 </Head>
-                <Main />
-                <NextScript />
+                <Main/>
+                <NextScript/>
             </Html>
         );
     }
